cdaebe894f849673966685215ba6262cd0f04715
riidefi <34194588+riidefi@users.noreply.github.com>
2019-09-27 19:06:10 -0400

    :art: Isolated environment specific includes.

--- TestEditor/source/main.cpp
+++ TestEditor/source/main.cpp
@@ -1,18 +1,4 @@
-/// Must include to properly build (tested in windows, vs2019)
-//////////////////////////////////////////////////////////////////////
-#if _WIN32
-#ifndef WIN32_LEAN_AND_MEAN
-#   define WIN32_LEAN_AND_MEAN 1
-#endif
-#include <windows.h>
-#include <commdlg.h>
-#include <shlobj.h>
-#include <shellapi.h>
-#include <future>
-#endif
-//////////////////////////////////////////////////////////////////////
-
-#include <pfd/portable-file-dialogs.h>
+#include <FileDialogues.hpp>
 #include <fstream>
 
 #include <core/Applet.hpp>
